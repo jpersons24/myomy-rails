4545a09d618395ade8643a71ef0a6567ef095525
Jakob Persons <jakobep24@gmail.com>
2021-04-11 13:46:53 -0400

    create workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -10,9 +10,21 @@ class WorkoutsController < ApplicationController
       render json: workout
    end
 
+   # POST /create
+   def create
+      workout = Workout.create(workout_params)
+      render json: workout
+   end
+
    def destroy
       workout = Workout.find(params[:id])
       workout.destroy
+   end
+
+   private
+
+   def workout_params
+      params.permit(:date, :duration, :workout_type, :user_id)
    end
 
 end
